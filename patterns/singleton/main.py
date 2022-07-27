b8ee56a5bf687af902501ecb259fc416aacb358f
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-07-27 16:18:59 -0500

    Add singleton to type

--- patterns/singleton/main.py
+++ patterns/singleton/main.py
@@ -2,25 +2,26 @@ from dataclasses import dataclass
 from typing import Dict
 
 
-def is_singleton(cls):
+class Singleton(type):
     _instances: Dict = {}
 
-    def get_instance(*args, **kwargs):
-        if cls in _instances:
+    def __call__(cls, *args, **kwargs):
+
+        if cls in cls._instances:
             raise Exception(
-                "This class is a singleton. Use the already created instance"
-                " or delete it before instanciating a new one"
+                "This class is a singleton. Use the already created instance "
+                "or delete it before instanciating a new one"
             )
-        if cls not in _instances:
-            _instances[cls] = cls(*args, **kwargs)
-        return _instances[cls]
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(
+                *args, **kwargs
+            )
 
-    return get_instance
+        return cls._instances[cls]
 
 
-@is_singleton
 @dataclass
-class DatabaseManager:
+class DatabaseManager(metaclass=Singleton):
     database_url: str
 
     def connect(self) -> str:
