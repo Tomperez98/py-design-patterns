ec61a113e2a4af05e2baf03cb21483a3b5085f39
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-07-28 11:56:14 -0500

    Add BaseVehicle mixin

--- patterns/bridge/bridges/vehicle/base.py
+++ patterns/bridge/bridges/vehicle/base.py
@@ -4,16 +4,12 @@ from dataclasses import dataclass
 from patterns.bridge.strategies import IEngine
 
 
-class BaseVehicleMethods(ABC):
-    @abstractmethod
-    def how_you_ride(self) -> str:
-        pass
-
-
 @dataclass
-class BaseVehicleParams:
+class BaseVehicleParamsMixin:
     engine: IEngine
 
 
-class BaseVehicle(BaseVehicleMethods, BaseVehicleParams):
-    pass
+class BaseVehicle(ABC, BaseVehicleParamsMixin):
+    @abstractmethod
+    def how_you_ride(self) -> str:
+        pass
